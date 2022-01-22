8917b75c6d48b1b54e07e926c9d7db0924c01756
Fatih Mustafa Sağır <fms@hotmail.com.tr>
2022-01-22 03:53:16 +0300

    application-info-style

--- src/containers/ApplicationInfo/index.js
+++ src/containers/ApplicationInfo/index.js
@@ -51,7 +51,49 @@ function ApplicationInfo() {
 
   return (
     <section className={styles["application-info"]}>
-      <FormContainer width="20">X</FormContainer>
+      <FormContainer width="40">
+        <h2>Başvuru Bilgisi</h2>
+        <div className={styles["ai-table"]}>
+          <table>
+            <tr>
+              <td>Başvuru Kodu</td>
+              <td>{foundApplication.applicationCode}</td>
+            </tr>
+            <tr>
+              <td>Başvuru Tarihi</td>
+              <td>ss</td>
+            </tr>
+            <tr>
+              <td>Başvuru Yapan Kişi</td>
+              <td>
+                {`${foundApplication.firstName} ${foundApplication.lastName}`}
+              </td>
+            </tr>
+            <tr>
+              <td>TC</td>
+              <td>{foundApplication.tc}</td>
+            </tr>
+            <tr>
+              <td>Doğum Tarihi</td>
+              <td>{foundApplication.age}</td>
+            </tr>
+            <tr>
+              <td>Adres</td>
+              <td>
+                {`${foundApplication.address} ${foundApplication.city} ${foundApplication.district}`}
+              </td>
+            </tr>
+            <tr>
+              <td>Başvuru Sebebi</td>
+              <td>{foundApplication.reason}</td>
+            </tr>
+            <tr>
+              <td>Diğer</td>
+              <td>{foundApplication.other}</td>
+            </tr>
+          </table>
+        </div>
+      </FormContainer>
     </section>
   );
 }
