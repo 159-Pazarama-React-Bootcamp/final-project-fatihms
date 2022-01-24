887e619c3730802d947d480cfcf540a11ddb2d47
Fatih Mustafa Sağır <fms@hotmail.com.tr>
2022-01-24 15:54:25 +0300

    homepage-1

--- src/pages/HomePage/index.js
+++ src/pages/HomePage/index.js
@@ -1,39 +1,20 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import { Link } from "react-router-dom";
-
-import { useSelector, useDispatch } from "react-redux";
-import { fetchApplications } from "../../redux/ApplicationData/applicationDataSlice";
 
 import styles from "./styles.module.css";
 
 function HomePage() {
-  const dispatch = useDispatch();
-  const data = useSelector((state) => state.applicationData);
-  console.log(data);
-
-  useEffect(() => {
-    dispatch(fetchApplications());
-  }, [dispatch]);
-
-  /*eslint-disable */
-  const testt = useSelector((state) =>
-    state.applicationData.applications.find(
-      (application) => application.applicationCode === "laj90eripd"
-    )
-  );
-  console.log(testt);
-  /* eslint-enable */
-
-  console.log(data.applications);
-
   return (
     <div className={styles.homepage}>
       <main>
-        <Link to="/basvuru-olustur" className={styles["homepage-link"]}>
+        <Link to="/basvuru-olustur" className={styles["make-application-link"]}>
           <button type="button">Başvuru Oluştur</button>
         </Link>
-        <Link to="/basvuru-sorgula" className={styles["homepage-link"]}>
+        <Link
+          to="/basvuru-sorgula"
+          className={styles["application-status-link"]}
+        >
           <button type="button">Başvuru Sorgula</button>
         </Link>
       </main>
